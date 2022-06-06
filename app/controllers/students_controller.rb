9435efa117f22b4afed1ed183cff493f5856f5d8
Jessica <jess.newman19@gmail.com>
2022-06-06 10:52:22 -0400

    commit tree f4fdab329ce4d67bc03e93a69253c9763b106b56

---
+++ app/controllers/students_controller.rb
@@ -0,0 +1,11 @@
+class StudentsController < ApplicationController
+  def index
+    students = Student.all 
+    render json: students
+  end
+
+  def grades 
+    grades = Student.all.order('grade DESC')
+    render json: grades
+  end
+end
